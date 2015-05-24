2a6281046448c19f128f1c79cf3dd3ef833e8f61
teranine <teranine@gmail.com>
2015-05-24 13:32:28 -0700

    files

---
+++ install_rtorrent.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+USER="rtorrent"
+USERHOME="/home/$USER"
+
+sudo apt-get update
+
+sudo apt-get install -y git-core subversion build-essential automake libtool libcppunit-dev libcurl4-openssl-dev libsigc++-2.0-dev libncurses5-dev zip rar unrar apache2 apache2-utils php5 php5-curl php5-geoip python-cheetah mediainfo libav-tools
+
+# Build and install xmlrpc from svn
+
+svn checkout http://svn.code.sf.net/p/xmlrpc-c/code/advanced xmlrpc-c
+cd xmlrpc-c
+./configure
+make
+sudo make install
+cd ..
+
+# Build and install libtorent
+
+# sudo git clone https://github.com/rakshasa/libtorrent.git
+tar -xzf libtorrent-0.13.4.tar.gz
+cd libtorrent-0.13.4
+./autogen.sh
+./configure
+make
+sudo make install
+cd ..
+
+# Build and install rtorrent
+
+# sudo git clone https://github.com/rakshasa/rtorrent.git
+tar -xzf rtorrent-0.9.4.tar.gz
+cd rtorrent-0.9.4
+./autogen.sh
+./configure --with-xmlrpc-c
+make
+sudo make install
+sudo ldconfig
+cd ..
+
+mkdir -p $USERHOME/{.session,watch,downloads}
+
+cp ./rtorrent.rc $USERHOME/.rtorrent.rc
+
+chmod 777 $USERHOME/{.session,watch,downloads,.rtorrent.rc}
